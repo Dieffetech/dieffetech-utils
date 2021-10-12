e12a383c352f89d2e52f5eda6109c55b2eb19266
Kristian Lentino <kristian.lentino@dieffetech.it>
2021-10-12 09:41:11 +0200

    Create RegexUtil.php

---
+++ src/RegexUtil.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Kristianlentino\DieffetechUtils;
+
+class RegexUtil
+{
+
+	const REGEX_VALID_DATETIME = '/^(\d{2})\/(\d{2})\/(\d{4}) (\d{2}):(\d{2})$/';
+	const REGEX_SECURE_PASSWORD = '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!;@$%^&*-]).{8,}$/';
+	const REGEX_VALID_DATE = '/^(\d{2})\/(\d{2})\/(\d{4})$/';
+	const REGEX_VALID_URL = '/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(?::\d{1,5})?(?:$|[?\/#])/i';
+}
