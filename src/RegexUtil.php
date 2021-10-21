a7093870f29b961fbc8c1c7953772dcb2058bdd7
Kristian Lentino <kristian.lentino@dieffetech.it>
2021-10-21 15:30:05 +0200

    aggiunta regex per controllo validità campi telefonici

--- src/RegexUtil.php
+++ src/RegexUtil.php
@@ -9,4 +9,5 @@ class RegexUtil
 	const REGEX_SECURE_PASSWORD = '/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!;@$%^&*-]).{8,}$/';
 	const REGEX_VALID_DATE = '/^(\d{2})\/(\d{2})\/(\d{4})$/';
 	const REGEX_VALID_URL = '/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(?::\d{1,5})?(?:$|[?\/#])/i';
+	const REGEX_VALID_PHONE = '^(\+\d{12}|\d{11}|\+\d{2}-\d{3}-\d{7})$';
 }
