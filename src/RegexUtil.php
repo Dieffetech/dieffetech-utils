16d00c0fcc0efb51a20c57f34cd79db9e408451d
Kristian Lentino <kristian.lentino@dieffetech.it>
2021-12-21 10:13:53 +0100

    Update RegexUtil.php

--- src/RegexUtil.php
+++ src/RegexUtil.php
@@ -10,6 +10,7 @@ class RegexUtil
 	const REGEX_VALID_DATE_ITALIAN = '/^(\d{2})\/(\d{2})\/(\d{4})$/';
 	const REGEX_VALID_DATE_AMERICAN = '/^(\d{4})\/(\d{2})\/(\d{2})$/';
 	const REGEX_VALID_URL = '/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(?::\d{1,5})?(?:$|[?\/#])/i';
+	const REGEX_ONLY_LETTERS = '/^[a-zA-Z]+$/';
 
 	public static function matchItalianDate(string $date)
 	{
