4c8c2d3be3023225fe346d157e08c32eeb991fb5
Kristian Lentino <kristian.lentino@dieffetech.it>
2022-03-04 17:24:37 +0100

    aggiunta funzione di compressione qualità immagini con imagick

--- src/ImageUtil.php
+++ src/ImageUtil.php
@@ -22,4 +22,44 @@ class ImageUtil
 		return $left.':'. $right;
 
 	}
+	public static function compressImage(int $quality,string $imagePath,string $destinationPath)
+	{
+		$img = new \Imagick($imagePath);
+		$size = $img->getImageLength();
+		$sizeInMb = round($size / pow(10,6),2);
+
+		if($sizeInMb >= 0.5){
+
+			/*$savedPath = ImageUtil::compress($imagePath,$baseFolder.'/'.$folder."/reduced_$index.jpeg",80);*/
+
+			$orientation = $img->getImageOrientation();
+
+			switch($orientation) {
+				case \Imagick::ORIENTATION_BOTTOMRIGHT:
+					$img->rotateimage("#000", 180); // rotate 180 degrees
+					break;
+
+				case \Imagick::ORIENTATION_RIGHTTOP:
+					$img->rotateimage("#000", 90); // rotate 90 degrees CW
+					break;
+
+				case \Imagick::ORIENTATION_LEFTBOTTOM:
+					$img->rotateimage("#000", -90); // rotate 90 degrees CCW
+					break;
+			}
+
+
+			// Now that it's auto-rotated, make sure the EXIF data is correct in case the EXIF gets saved with the image!
+			$img->setImageCompression(\Imagick::COMPRESSION_JPEG);
+			$img->setImageCompressionQuality($quality);
+			$img->setImageBackgroundColor('#ffffff');
+			$img->stripImage();
+			$img->setImageAlphaChannel(\Imagick::ALPHACHANNEL_REMOVE);
+			$img = $img->mergeImageLayers(\Imagick::LAYERMETHOD_FLATTEN);
+			$img->setImageOrientation(\Imagick::ORIENTATION_TOPLEFT);
+			return [
+				'success' => $img->writeImage($destinationPath)
+			];
+		}
+	}
 }
